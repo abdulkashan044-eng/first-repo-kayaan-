965d611eb94b3b5b6f081b8392356af8c2fad29d
samikshap2212 <samikshapawar957@gmail.com>
2025-08-22 22:17:17 -0700

    first commit

---
+++ mathop.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return(x+y)
+def diff(x,y):
+    return(x-y)
+def product(x,y):
+    return(x*y)
+def div(x,y):
+    return(x/y)
---
+++ module.py
@@ -0,0 +1,7 @@
+import mathop as m
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+print=("addition")m.add(num1,num2,add)
+print=("substraction")m.diff(num1,num2diff)
+print=("multiplication")m.diff(num1,num2,product)
+print=("division")m.diff(num1,num2,div)
